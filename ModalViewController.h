43233e3bc17d6e54b25beda49603116f7ef19774
Joey <Joey@abcs-macbook-pro.local>
2015-10-02 16:20:58 -0500

    Added Modal View controller, and functionality to allow go back to the map screen

---
+++ ModalViewController.h
@@ -0,0 +1,14 @@
+//
+//  Modal.h
+//  UbiqProject
+//
+//  Created by Joey on 10/2/15.
+//  Copyright © 2015 Joey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ModalViewController : UIViewController
+- (IBAction)BackButton:(id)sender;
+
+@end
---
+++ ModalViewController.m
@@ -0,0 +1,40 @@
+//
+//  Modal.m
+//  UbiqProject
+//
+//  Created by Joey on 10/2/15.
+//  Copyright © 2015 Joey. All rights reserved.
+//
+
+#import "ModalViewController.h"
+
+@interface ModalViewController ()
+
+@end
+
+@implementation ModalViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)BackButton:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+@end
