791d62b72b47bb8093fd97cef60ccaeb3ab81f9f
robert-vo <robertvo79@gmail.com>
2015-10-18 23:30:28 -0500

    Changed view controller to map view controller.

---
+++ UbiqProject/MapViewController.h
@@ -0,0 +1,17 @@
+//
+//  MapViewController.h
+//  UbiqProject
+//
+//  Created by Robert Vo on 10/18/15.
+//  Copyright © 2015 Joey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+#import "Query.h"
+
+@interface MapViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet MKMapView *MapView;
+
+@end
---
+++ UbiqProject/MapViewController.m
@@ -0,0 +1,54 @@
+//
+//  MapViewController.m
+//  UbiqProject
+//
+//  Created by Robert Vo on 10/18/15.
+//  Copyright © 2015 Joey. All rights reserved.
+//
+
+#import "MapViewController.h"
+#import "Query.h"
+
+#define METERS_PER_MILE 1609.344
+
+@interface MapViewController ()
+
+@end
+
+@implementation MapViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"💩");
+    
+    // 1
+    CLLocationCoordinate2D zoomLocation;
+    zoomLocation.latitude = 29.817178;
+    zoomLocation.longitude= -95.4012915;
+    
+    // 2
+    MKCoordinateRegion viewRegion = MKCoordinateRegionMakeWithDistance(zoomLocation, 0.5*METERS_PER_MILE, 0.5*METERS_PER_MILE);
+    
+    // 3
+    [_MapView setRegion:viewRegion animated:YES];
+
+    
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
