cf770b9ece5dde4afac86868f330e148e8c2e561
robert-vo <robertvo79@gmail.com>
2015-10-18 23:25:39 -0500

    Added query object

---
+++ UbiqProject/Query.h
@@ -0,0 +1,16 @@
+//
+//  Query.h
+//  UbiqProject
+//
+//  Created by Robert Vo on 10/18/15.
+//  Copyright © 2015 Joey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Query : NSObject
+
+@property NSString *category;
+@property NSMutableArray *locations;
+
+@end
---
+++ UbiqProject/Query.m
@@ -0,0 +1,15 @@
+//
+//  Query.m
+//  UbiqProject
+//
+//  Created by Robert Vo on 10/18/15.
+//  Copyright © 2015 Joey. All rights reserved.
+//
+
+#import "Query.h"
+
+@implementation Query
+
+@synthesize category, locations;
+
+@end
