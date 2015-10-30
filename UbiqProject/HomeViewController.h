73c715c3410e11259f57f1a1b0865b07300b2be7
robert-vo <robertvo79@gmail.com>
2015-10-30 01:59:48 -0500

    Added current location delegate and manager.

--- UbiqProject/HomeViewController.h
+++ UbiqProject/HomeViewController.h
@@ -10,22 +10,22 @@
 #import <MapKit/MapKit.h>
 #import "Query.h"
 
-@interface HomeViewController : UIViewController <UITextFieldDelegate>
+@interface HomeViewController : UIViewController <UITextFieldDelegate, CLLocationManagerDelegate>
+
 
 @property (weak, nonatomic) IBOutlet UITextField *FirstLocation;
 @property (weak, nonatomic) IBOutlet UITextField *SecondLocation;
 @property (weak, nonatomic) IBOutlet UITextField *CommonInterestPoints;
-
 @property int direction;
 @property int shakes;
 @property Query *queryToPass;
 @property (weak, nonatomic) IBOutlet UISwitch *FirstLocationSwitch;
+@property (strong, retain) CLLocationManager *currentLocationManager;
+@property BOOL locationFound;
 
 - (IBAction)ConvergeLocations:(id)sender;
-- (NSMutableArray*) getCoordinateLocations;
 - (BOOL) isValidLocationEntry:(NSString *) location;
 - (void) setUpKeyboardToDismissOnReturn;
-
 
 @end
 
