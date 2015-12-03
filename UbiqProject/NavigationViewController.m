56e9d299809ff599173f432996e0c72be4d44ad7
mlaiseca <Mario@marios-macbook-pro.local>
2015-12-03 01:39:39 -0600

    hamburger menu works but no functionality yet

---
+++ UbiqProject/NavigationViewController.m
@@ -0,0 +1,61 @@
+//
+//  NavigationViewController.m
+//  UbiqProject
+//
+//  Created by Mario Laiseca-Ruiz on 12/3/15.
+//  Copyright © 2015 Joey. All rights reserved.
+//
+
+#import "NavigationViewController.h"
+
+@interface NavigationViewController ()
+
+@end
+
+@implementation NavigationViewController{
+    NSArray *menu;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    menu = @[@"first", @"second", @"third", @"fourth", @"fifth"];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+
+    return [menu count];
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    NSString *cellIdentifier = [menu objectAtIndex:indexPath.row];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+
+
+@end
