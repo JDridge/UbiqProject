51af89cb25604a4c3722126ad1f945b0e5856b59
robert-vo <robertvo79@gmail.com>
2015-12-07 18:29:02 -0600

    Added method to get mailgun api keys.

---
+++ UbiqProject/Utilities.m
@@ -0,0 +1,13 @@
+//
+//  Utilities.m
+//  UbiqProject
+//
+//  Created by Robert Vo on 12/7/15.
+//  Copyright © 2015 Joey. All rights reserved.
+//
+
+#import "Utilities.h"
+
+@implementation Utilities
+
+@end
